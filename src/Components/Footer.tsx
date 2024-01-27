b25a6a381f13f5708056117b26ab7c1e5ce5cb3e
gio87 <gio.dz@icloud.com>
2024-01-26 19:55:52 -0500

    fix

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="bg-[#1B262F] w-full h-full flex flex-col items-center justify-center ">
       <div className="flex flex-col items-center px-5 justify-between w-full py-10 gap-10 md:flex-row xl:w-[1180px]">
         <div className="flex flex-col gap-10 md:flex-row ">
-          <img src="public/assets/shared/desktop/footerLogo.svg" alt="" />
+          <img src="./assets/shared/desktop/footerLogo.svg" alt="" />
           <div className=" text-[15px] font-bold text-[#FBFCFE] opacity-70 flex flex-col items-center justify-between gap-8 md:flex-row">
             <Link to={"/pricing"}>Pricing</Link>
 
